fc4d9e074ac562938058e2b3ab4461952e59a434
katudonn99 <24hs207@s.bgu.ac.jp>
2024-12-02 13:49:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
         function checkFortune() {
             var colour = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "凶"; // デフォルトの値
+            var result = "梅"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
+            if (colour === "朝" && number === 0) {
                 result = "しゃけ";
-            } else if (colour === "黒" && number === 1) {
+            } else if (colour === "朝" && number === 1) {
                 result = "いくら";
-            } else if (colour === "黒" && number === 2) {
+            } else if (colour === "朝" && number === 2) {
                 result = "昆布";
-            } else if (colour === "白" && number === 0) {
+            } else if (colour === "昼" && number === 0) {
                 result = "梅";
-            } else if (colour === "白" && number === 1) {
+            } else if (colour === "昼" && number === 1) {
                 result = "すじこ";
-            } else if (colour === "白" && number === 2) {
-                result = "塩むすび";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "そぼろ";
-            } else if (colour === "オレンジ" && number === 1) {
+            } else if (colour === "昼" && number === 2) {
                 result = "ツナマヨネーズ";
-            } else if (colour === "オレンジ" && number === 2) {
+            } else if (colour === "おやつに" && number === 0) {
+                result = "そぼろ";
+            } else if (colour === "おやつに" && number === 1) {
+                result = "塩むすび";
+            } else if (colour === "おやつに" && number === 2) {
                 result = "おかか";
             }
 
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+            document.getElementById('result-output').innerText = "今日のおにぎりの具材は【" + result + "】にしましょう!";
         }
