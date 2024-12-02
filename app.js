a291b0a4c4fa31cdd0486b50314ee8ce9123dfdd
katudonn99 <24hs207@s.bgu.ac.jp>
2024-12-02 13:42:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,23 @@
             var result = "凶"; // デフォルトの値
 
             if (colour === "黒" && number === 0) {
-                result = "小吉";
+                result = "しゃけ";
             } else if (colour === "黒" && number === 1) {
-                result = "中吉";
+                result = "いくら";
             } else if (colour === "黒" && number === 2) {
-                result = "凶";
+                result = "昆布";
             } else if (colour === "白" && number === 0) {
-                result = "中吉";
+                result = "梅";
             } else if (colour === "白" && number === 1) {
-                result = "中吉";
+                result = "すじこ";
             } else if (colour === "白" && number === 2) {
-                result = "大凶";
+                result = "塩むすび";
             } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
+                result = "そぼろ";
             } else if (colour === "オレンジ" && number === 1) {
-                result = "大吉";
+                result = "ツナマヨネーズ";
             } else if (colour === "オレンジ" && number === 2) {
-                result = "中吉";
+                result = "おかか";
             }
 
             document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
