c6df57babbb097c2fe02fb874938ddcf09fd7800
katudonn99 <24hs207@s.bgu.ac.jp>
2024-12-20 16:51:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
         function checkFortune() {
             var time = document.getElementById('time-select').value;
             var sidemenu = document.getElementById('sidemenu-select').value,;    
-            var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "梅"; // デフォルトの値
 
-            if (time === "朝" &&& sidemenu === "汁物"&&& number === 0) {
+            var result = "梅とみそ汁"; // デフォルトの値
+
+            if (time === "朝" &&&sidemenu === "汁物") {
                 result = "しゃけとみそ汁";
-            } else if (time === "朝" &&& sidemenu === "サラダ"&&& number === 0) {
+            } else if (time === "朝" && sidemenu === "サラダ") {
                 result = "しゃけとコールスロー";    
-            } else if (time === "朝"&&& sidemenu === "汁物" &&& number === 1) {
+            } else if (time === "朝"&& sidemenu === "汁物" ) {
                 result = "いくらと豚汁";
-            }   else if (time === "朝"&&& sidemenu === "サラダ" &&& number === 1) {
+            }   else if (time === "朝"&& sidemenu === "サラダ" ) {
                 result = "いくらとポテトサラダ";
-            } else if (time === "朝"  &&& sidemenu === "汁物" &&& number === 2) {
+            } else if (time === "朝"  && sidemenu === "汁物" ) {
                 result = "昆布とコンソメスープ";
-            } else if (time === "朝" &&& sidemenu === "サラダ" &&& number === 2) {
+            } else if (time === "朝" && sidemenu === "サラダ" ) {
                 result = "昆布とマカロニサラダ";
-            } else if (time === "昼"&&& sidemenu === "汁物"  &&& number === 0) {
+            } else if (time === "昼"&& sidemenu === "汁物" ) {
                 result = "梅と野菜スープ";
-            } else if (time === "昼"&&& sidemenu === "サラダ"  &&& number === 0) {
+            } else if (time === "昼"&& sidemenu === "サラダ") {
                 result = "梅とレタスサラダ(胡麻ドレッシング)";
-            }else if (time === "昼"&&& sidemenu === "汁物"  &&& number === 1) {
+            }else if (time === "昼"&& sidemenu === "汁物") {
                 result = "すじことオニオングラタンスープ";
-            } else if (time === "昼"&&& sidemenu === "サラダ"  &&& number === 1) {
+            } else if (time === "昼"&& sidemenu === "サラダ") {
                 result = "すじことオニオンサラダ(イタリアンドレッシング)";
-            }else if (time === "昼" &&& sidemenu === "汁物" &&& number === 2) {
+            }else if (time === "昼" && sidemenu === "汁物") {
                 result = "ツナマヨネーズとミネストローネ";
-            } else if (time === "昼" &&& sidemenu === "サラダ" &&& number === 2) {
+            } else if (time === "昼" && sidemenu === "サラダ") {
                 result = "ツナマヨネーズとレタスサラダ(青じそドレッシング)";
-            } else if (time === "おやつに"&&& sidemenu === "汁物"  &&& number === 0) {
+            } else if (time === "おやつに"&& sidemenu === "汁物" ) {
                 result = "そぼろとすいとん";
-            } else if (time === "おやつに"&&& sidemenu === "サラダ"  &&& number === 0) {
+            } else if (time === "おやつに"&& sidemenu === "サラダ" ) {
                 result = "そぼろとオニオンサラダ(和風ドレッシング)";
-            } else if (time === "おやつに"&&& sidemenu === "汁物"  &&& number === 1) {
+            } else if (time === "おやつに"&& sidemenu === "汁物") {
                 result = "塩むすびとお吸い物";
             } else if (time === "おやつに"&&& sidemenu === "サラダ"  &&& number === 1) {
                 result = "塩むすびとチョレギサラダ";
-            }else if (time === "おやつに" &&& sidemenu === "汁物" &&& number === 2) {
+            }else if (time === "おやつに" && sidemenu === "汁物") {
                 result = "おかかとクラムチャウダー";
-            }else if (time === "おやつに" &&& sidemenu === "サラダ" &&& number === 2) {
+            }else if (time === "おやつに" && sidemenu === "サラダ" ) {
                 result = "おかかとフルーツサラダ";
             }
 
