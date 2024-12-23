6e102df426a4ae7c231f4099e62c6bc2ff42d056
katudonn99 <24hs207@s.bgu.ac.jp>
2024-12-23 13:18:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,41 +3,41 @@ function checkFortune() {
         　　 var number = parseInt(document.getElementById('number-input').value, 10);
              var result = "梅とみそ汁"; // デフォルトの値
 
-            if (time === "朝" && number === １) {
+            if (time === "朝" && number ===0 ) {
                 result = "しゃけおにぎりとみそ汁";
-            } else if (time === "朝" && number === 2) {
+            } else if (time === "朝" && number ===1) {
                 result = "しゃけおにぎりとコールスロー";    
-            } else if (time === "朝"&& number === 3) {
+            } else if (time === "朝"&& number === 2) {
                 result = "いくらおにぎりと豚汁";
-            }   else if (time ==="朝" && number === 4 ) {
+            }   else if (time ==="朝" && number === 3 ) {
                 result = "いくらおにぎりとポテトサラダ";
-            } else if (time === "朝"  && number === 5) {
+            } else if (time === "朝"  && number === 4) {
                 result = "昆布おにぎりとコンソメスープ";
-            } else if (time === "朝" && number === 6) {
+            } else if (time === "朝" && number === 5) {
                 result = "昆布おにぎりとマカロニサラダ";
-            } else if (time === "昼"&& number === 1) {
+            } else if (time === "昼"&& number === 0) {
                 result = "梅おにぎりと野菜スープ";
-            } else if (time === "昼"&& number === 2) {
+            } else if (time === "昼"&& number === 1) {
                 result = "梅おにぎりとレタスサラダ(胡麻ドレッシング)";
-            }else if (time === "昼"&& number === 3) {
+            }else if (time === "昼"&& number === 2) {
                 result = "すじこおにぎりとオニオングラタンスープ";
-            } else if (time === "昼"&& number === 4") {
+            } else if (time === "昼"&& number === 3) {
                 result = "すじこおにぎりとオニオンサラダ(イタリアンドレッシング)";
-            }else if (time === "昼" && number === 5) {
+            }else if (time === "昼" && number === 4) {
                 result = "ツナマヨネーズおにぎりとミネストローネ";
-            } else if (time === "昼"&& number === 6) {
+            } else if (time === "昼"&& number === 5) {
                 result = "ツナマヨネーズおにぎりとレタスサラダ(青じそドレッシング)";
-            } else if (time === "おやつに"&& number === 1 ) {
+            } else if (time === "おやつに"&& number === 0) {
                 result = "そぼろおにぎりとすいとん";
-            } else if (time === "おやつに"&& number === 2 ) {
+            } else if (time === "おやつに"&& number === 1) {
                 result = "そぼろおにぎりとオニオンサラダ(和風ドレッシング)";
-            } else if (time === "おやつに"&& number === 3) {
+            } else if (time === "おやつに"&& number === 2) {
                 result = "塩むすびとお吸い物";
-            } else if (time === "おやつに"&& number === 4) {
+            } else if (time === "おやつに"&& number === 3) {
                 result = "塩むすびとチョレギサラダ";
-            }else if (time === "おやつに" && number === 5) {
+            }else if (time === "おやつに" && number === 4) {
                 result = "おかかおにぎりとクラムチャウダー";
-            }else if (time === "おやつに" && number === 6 ) {
+            }else if (time === "おやつに" && number === 5 ) {
                 result = "おかかおにぎりとフルーツサラダ";
             }
 
